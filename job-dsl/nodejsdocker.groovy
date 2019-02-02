32840c1a08c8b0f09913070f401a7877c4929db0
H.G. Min <hgmin777@naver.com>
2019-02-03 00:41:51 +0900

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -15,7 +15,7 @@ job('NodeJS Docker example') {
     steps {
         dockerBuildAndPublish {
             repositoryName('hgmin777/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
+            tag('latest')    //tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
